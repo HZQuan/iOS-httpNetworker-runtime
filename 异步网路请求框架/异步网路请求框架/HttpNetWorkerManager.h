d8857e8e05ef181e0a03d72fa46367a62a54de2a
huangzengquan <1634104309.@qq.com>
2015-12-05 10:40:50 +0800

    “首次提交”

---
+++ 异步网路请求框架/异步网路请求框架/HttpNetWorkerManager.h
@@ -0,0 +1,22 @@
+//
+//  HttpNetWorkerManager.h
+//  异步网路请求框架
+//
+//  Created by 黄增权 on 15/10/28.
+//  Copyright © 2015年 黄增权. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HttpRequest.h"
+#import "HttpResponse.h"
+typedef void(^responseblock)(HttpResponse *response);
+
+@interface HttpNetWorkerManager : NSObject
++(instancetype) manager;
+-(void) requestWithRequeter:(HttpRequest *)request response:(responseblock) responseblock;
+
+
+
+
+
+@end
