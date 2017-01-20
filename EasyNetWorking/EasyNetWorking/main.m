5a1524ccc59126bf257fb3e5659ce51020d3c520
quan <1634104309@qq.com>
2017-01-20 22:24:07 +0800

    ..

---
+++ EasyNetWorking/EasyNetWorking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyNetWorking
+//
+//  Created by quan on 17/1/20.
+//  Copyright © 2017年 quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
